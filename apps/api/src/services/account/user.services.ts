1ba35dee21324f9b8643756a5a06644f7dc3b09f
RadidDesafandri <radietdesfandri@gmail.com>
2025-03-30 19:37:53 +0700

    fix: change and refactor code

--- apps/api/src/services/account/user.services.ts
+++ apps/api/src/services/account/user.services.ts
@@ -11,7 +11,9 @@ import path from 'path';
 import fs from 'fs';
 import handlebars from 'handlebars';
 const secret = process.env.SECRET_KEY || 'nezztar';
-const base_url = process.env.BASE_URL_BACKEND || 'http://localhost:8000/api';
+const base_url =
+  process.env.BASE_URL_BACKEND ||
+  'https://property-rent-app-one.vercel.app/api';
 const imageurl = process.env.BASE_URL_BACKEND + '/public/welcome2.svg';
 const imageurl2 = process.env.BASE_URL_BACKEND + '/public/forgotpassmail.svg';
 const imageurl3 = process.env.BASE_URL_BACKEND + '/public/changeemail.svg';
@@ -145,8 +147,7 @@ export const forgotPasswordUserServices = async (email: string) => {
     );
     const token = createToken(payload, '30m');
     const link =
-      process.env.BASE_URL_FRONTEND +
-      `/account/forgot-password-user/${token}`;
+      process.env.BASE_URL_FRONTEND + `/account/forgot-password-user/${token}`;
     const dataEmail = {
       link,
       username: user.username ? user.username : user.email,
